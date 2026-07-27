e41b95b4679c1d9ce8c8ac2416504430d1234ef8
jeevan <jeevansandhu1507@gmail.com>
2026-07-28 01:43:41 +0530

    added Day38 and Day39

---
+++ Day39/backend/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Backend is running successfully!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
